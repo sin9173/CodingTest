92070a64965fac7569162e915a752c9e3a61609d
sin9173 <54428255+sin9173@users.noreply.github.com>
2022-09-05 17:59:13 +0900

    소트사이트, 좌표정렬

--- src/k/SortInsight.java
+++ src/k/SortInsight.java
@@ -12,12 +12,41 @@ public class SortInsight {
     public static void main(String[] args) throws IOException {
         init();
         printArray(nums);
-        nums = quickSort(nums, 0, nums.length);
+        nums = quickSortReverse(nums, 0, nums.length-1);
     }
 
     public static void init() throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         nums = Stream.of(bf.readLine().split("")).map(s -> Integer.parseInt(s)).toArray(Integer[]::new);
+    }
+
+    public static Integer[] quickSortReverse(Integer[] array, Integer originLeft, Integer originRight) {
+        int left = originLeft;
+        int right = originRight;
+        int pivot = array[originLeft + ((originRight-originLeft)/2)];
+
+        int temp;
+
+        printArray(array);
+        System.out.println("originLeft : " + originLeft);
+        System.out.println("originRight : " + originRight);
+        System.out.println("pivot : " + pivot);
+        while(left<=right) {
+            while(array[left]>pivot) left++;
+            while(array[right]<pivot) right--;
+
+            if(left<=right) {
+                temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+                left++;
+                right--;
+                printArray(array);
+            }
+        }
+        if(originLeft<right) array = quickSortReverse(array, originLeft, right);
+        if(originRight>left) array = quickSortReverse(array, left, originRight);
+        return array;
     }
 
     public static Integer[] quickSort(Integer[] array, Integer originLeft, Integer originRight) {
@@ -28,8 +57,8 @@ public class SortInsight {
         int temp;
 
         while(left<=right) {
-            while(left<=pivot) left++;
-            while(right>=pivot) right--;
+            while(array[left]<=pivot) left++;
+            while(array[right]>=pivot) right--;
 
             if(left<=right) {
                 temp = array[left];
