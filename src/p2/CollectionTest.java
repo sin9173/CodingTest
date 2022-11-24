2cefbbcff2dd69f98734b2ebe8a6990b0008ffb2
sin9173 <54428255+sin9173@users.noreply.github.com>
2022-11-24 18:06:41 +0900

    PhoneList

---
+++ src/p2/CollectionTest.java
@@ -0,0 +1,15 @@
+package p2;
+
+import java.util.ArrayList;
+
+public class CollectionTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void test(ArrayList<String> list) {
+        list.clear();
+        list = null;
+    }
+}
